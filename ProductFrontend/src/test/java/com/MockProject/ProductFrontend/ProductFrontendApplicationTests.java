fb1110ed0b5bea982822a713324b91e7d3e0cd34
Kiran Tash Nariansamy <kirantashnariansamy@gmail.com>
2024-11-19 08:37:02 +0200

    Added Boilerplate spring code for front end from start.spring.io

---
+++ ProductFrontend/src/test/java/com/MockProject/ProductFrontend/ProductFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MockProject.ProductFrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
