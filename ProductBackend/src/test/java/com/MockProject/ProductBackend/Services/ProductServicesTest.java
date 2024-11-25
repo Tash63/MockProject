3fd748192495a9cbdf1bf747a2be05345a080417
Kiran Tash Nariansamy <kirantashnariansamy@gmail.com>
2024-11-25 10:29:57 +0200

    Added Jacocco Dependencies and added test classes for the services

---
+++ ProductBackend/src/test/java/com/MockProject/ProductBackend/Services/ProductServicesTest.java
@@ -0,0 +1,43 @@
+package com.MockProject.ProductBackend.Services;
+
+import com.MockProject.ProductBackend.Data.Model.Product;
+import com.MockProject.ProductBackend.Data.Repositories.ProductRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServicesTest {
+
+    @Mock
+    private ProductRepository productRepository;
+    @InjectMocks
+    private ProductServices productServices;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllProducts() {
+        List<Product> Products = new ArrayList<>();
+        Products.add(new Product(Long.getLong("1"), "Speedpoint", Double.valueOf(400)));
+        Products.add(new Product(Long.getLong("2"), "Speedee", Double.valueOf(200)));
+        Products.add(new Product(Long.getLong("3"), "SpeedeeQR", Double.valueOf(100)));
+        when(productRepository.findAll()).thenReturn(Products);
+        assertIterableEquals(Products, productServices.getAllProducts());
+    }
+}
---
+++ ProductBackend/src/test/java/com/MockProject/ProductBackend/Services/ProductUserServicesTest.java
@@ -0,0 +1,16 @@
+package com.MockProject.ProductBackend.Services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductUserServicesTest {
+
+    @Test
+    void saveProductToUser() {
+    }
+
+    @Test
+    void getProductsByCustomer() {
+    }
+}
